c0f30b3d7b758b86a92079f193ef6150469726e9
Pedro Duarte <oi@ped.ro>
2022-05-17 21:04:21 +0200

    ui

--- components/Mint.tsx
+++ components/Mint.tsx
@@ -4,20 +4,17 @@ import { useContractWrite } from 'wagmi';
 import contractInterface from '../contract-abi.json';
 
 export function Mint({ ready }: any) {
-  const [minted, setMinted] = React.useState(false);
-
   // 📄 Contract Config
   const contractConfig = {
     addressOrName: '0xf175a8bb93c63dfd6416964882dfef9f27db4a9a',
     contractInterface: contractInterface,
   };
 
-  const { write } = useContractWrite(contractConfig, 'mint');
-
-  const mint = () => {
-    write();
-    setMinted(true);
-  };
+  const {
+    write: mint,
+    isLoading,
+    isSuccess,
+  } = useContractWrite(contractConfig, 'mint');
 
   return (
     <div className="card">
@@ -32,8 +29,14 @@ export function Mint({ ready }: any) {
       <div className="content">
         <h1>RainbowKit NFT</h1>
         {ready ? (
-          <button disabled={minted} className="button" onClick={() => mint()}>
-            {minted ? 'Minted' : 'Mint'}
+          <button
+            disabled={isLoading || isSuccess}
+            className="button"
+            data-minting={isLoading}
+            data-minted={isSuccess}
+            onClick={() => mint()}
+          >
+            {isSuccess ? 'Minted' : isLoading ? 'Minting...' : 'Mint'}
           </button>
         ) : (
           <p>Connect your wallet to mint.</p>
